890fc2791ba58635cfafb400e54fc16ba1e758d1
Shivam Kumar <122041599+shivamkumar6120@users.noreply.github.com>
2025-11-14 12:51:53 +0530

    user singup bcrypt final

--- routes/user.js
+++ routes/user.js
@@ -30,25 +30,21 @@ router.get("/", (req, res) => {
 
 router.post("/signup",async (req, res) => {
 
-    // 1. Destructure using the key name sent from Postman: 'birth'
+
     const { first_name, last_name, email, password, mobile, birth } = req.body 
     
-    // 2. Update the SQL query to use the column name your table expects
-    //    (Assuming your database column is named `birth` or `birth_date`)
-    //    Based on prior context, let's assume the column is named `birth_date`:
+
     const sql = `INSERT INTO USERS(first_name, last_name, email, password, mobile, birth) VALUES(?, ?, ?, ?, ?, ?)`
 
     console.log(req.body)
     try{
         const hashedPassword = await bcrypt.hash(password, config.saltRound) 
         
-        // 3. Ensure your parameters array uses the correct variables
         pool.query(sql,[first_name, last_name, email, hashedPassword, mobile, birth],(error,data)=>{
             if(data){
                 res.send({status : "Success", data})
             }
             else{
-                // This block is hit because the SQL query failed due to the mismatch
                 res.send({status : "Error", error}) 
             }
         })
@@ -57,24 +53,7 @@ router.post("/signup",async (req, res) => {
     }
 })
 
-//user singup
-// router.post("/signup",(req,res)=>{
 
-//       const { first_name, last_name, email, password, mobile, birth_date } = req.body
-
-//     const sql = "INSERT INTO USERS(first_name, last_name, email, password, mobile, birth) VALUES(?,? ,? ,? ,? ,?)"
-
-//     pool.query(sql,[first_name, last_name, email, password, mobile, birth_date],(error,data)=>{
-//         if(data){
-
-//             res.send({status : "success", data})
-//         }
-//         else{
-//             res.send({status : "Error", error})
-//         }
-//     })
-
-// })
 
 
 module.exports = router;
